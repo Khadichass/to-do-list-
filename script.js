c2b027152194feaefc4409710f4e7c47546b66ca
Khadicha <246abdurashidova123@gmail.com>
2023-03-15 16:42:36 +0500

    todo list created

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-"use script";
-
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
-document.addEventListener("DOMContentLoaded", getLocalTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("change", filterTodo);
